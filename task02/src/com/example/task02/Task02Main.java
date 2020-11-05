04c413861b4aae019aa3724472c0ba77772a388f
redrockstyle <ulowpie@gmail.com>
2020-11-05 18:53:01 +0500

    [Khazov] task02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,6 +1,8 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
@@ -9,5 +11,24 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+        convertEndOfLine(System.in, System.out);
     }
+
+    public static void convertEndOfLine(InputStream inputStream, OutputStream outputStream) throws IOException {
+        if (inputStream == null) {
+            throw new IllegalArgumentException("stream is null");
+        }
+        int code1 = inputStream.read(), code2;
+        while(code1 != -1){
+            code2 = inputStream.read();
+
+            if (!(code2 == 10 && code1 == 13)){
+                outputStream.write(code1);
+            }
+
+            code1 = code2;
+        }
+        outputStream.flush();
+    }
+
 }
